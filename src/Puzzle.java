974d1b293c7abc1c7e39309f40a364cc964f3fa9
Bruno Pedroso <Bruno@macasa.local>
2009-02-18 17:53:48 -0300

    [Bruno e Tulio] desfizemos o inicio de rewrite e refatoramos o metodo recursivo. refatoramos outras coisinhas.

--- src/Puzzle.java
+++ src/Puzzle.java
@@ -63,18 +63,8 @@ public class Puzzle {
 	}
 
 	public String preProcessGrid() {
-//		contents = contents.replace(" ", "");
-//
-//		// troca os X's na borda da direita e da esquerda
-//		contents = contents.replace("X\n", "B\n").replaceAll("\\nX", "\nB");
-
-		String[] linhas = contents.split("\n");
-
-//		// troca X por B na primeira e na œltima linha
-//		linhas[0]=linhas[0].replace("X", "B");
-//		linhas[linhas.length-1] = linhas[linhas.length-1].replace("X", "B");
-
-		char[][] tabuleiro = montarTabuleiro(linhas);
+		
+		char[][] tabuleiro = montarTabuleiro(contents);
 
 		int maxLin = tabuleiro.length - 1;
 		int maxCol = tabuleiro[0].length - 1;
@@ -82,76 +72,66 @@ public class Puzzle {
 		//recursivo
 		//primeira linha
 		for (int x = 0; x < maxCol; x++) {
-			mudarParaBranco(tabuleiro, 0, x);
-//			if (tabuleiro[0][x] == 'B') {
-//				verificaVizinhos(0, x, tabuleiro);
-//			}
+			if (tabuleiro[0][x] == 'X') {
+				trocaPraBranco(0, x, tabuleiro);
+			}
 		}
 
 		//ultima linha
 		for (int x = 0; x < maxCol; x++) {
-			if (tabuleiro[maxLin][x] == 'B') {
-				verificaVizinhos(maxLin, x, tabuleiro);
+			if (tabuleiro[maxLin][x] == 'X') {
+				trocaPraBranco(maxLin, x, tabuleiro);
 			}
 		}
 
 		//primeira coluna
 		for (int x = 0; x < maxLin; x++) {
-			if (tabuleiro[x][0] == 'B') {
-				verificaVizinhos(x, 0, tabuleiro);
+			if (tabuleiro[x][0] == 'X') {
+				trocaPraBranco(x, 0, tabuleiro);
 			}
 		}
 
 		//ultima coluna
 		for (int x = 0; x < maxLin; x++) {
-			if (tabuleiro[x][maxCol] == 'B') {
-				verificaVizinhos(x, maxCol, tabuleiro);
+			if (tabuleiro[x][maxCol] == 'X') {
+				trocaPraBranco(x, maxCol, tabuleiro);
 			}
 		}
 
 		return montarResultado(tabuleiro);
 	}
 
-	private void mudarParaBranco(char[][] tabuleiro, int linha, int coluna) {
-		if (tabuleiro[linha][coluna] == 'X' && (linha == 0 || linha == tabuleiro.length || coluna == 0 || coluna == tabuleiro[0].length)){
-			tabuleiro[linha][coluna] = 'B';
-			for (int i = linha-1; i <= linha+1; i++) {
-				for (int j = coluna-1; j <= coluna+1; j++) {
-					mudaParaBranco(tabuleiro, i, j);
-				}
-			}
-		}
+	private void trocaPraBranco(int linha, int coluna, char [][] tabuleiro) {
 		
-	}
-
-	private void verificaVizinhos(int linha, int coluna, char [][] tabuleiro) {
-
+		tabuleiro[linha][coluna] = 'B';
+		
 		//Verifica direita
 		if (coluna+1 < tabuleiro[linha].length && tabuleiro[linha][coluna+1] == 'X') {
-			tabuleiro[linha][coluna+1] = 'B';
-			verificaVizinhos (linha, coluna+1, tabuleiro);
+			trocaPraBranco (linha, coluna+1, tabuleiro);
 		}
 
 		//Verifica esquerda
 		if (coluna-1 > 0 && tabuleiro[linha][coluna-1] == 'X') {
-			tabuleiro[linha][coluna-1] = 'B';
-			verificaVizinhos (linha, coluna-1, tabuleiro);
+			trocaPraBranco (linha, coluna-1, tabuleiro);
 		}
 
 		//Verifica embaixo
 		if (linha+1 < tabuleiro.length && tabuleiro[linha+1][coluna] == 'X') {
-			tabuleiro[linha+1][coluna] = 'B';
-			verificaVizinhos (linha+1, coluna, tabuleiro);
+			trocaPraBranco (linha+1, coluna, tabuleiro);
 		}
 
 		//Verifica "cima"
 		if (linha-1 > 0 && tabuleiro[linha-1][coluna] == 'X') {
-			tabuleiro[linha-1][coluna] = 'B';
-			verificaVizinhos (linha-1, coluna, tabuleiro);
+			trocaPraBranco (linha-1, coluna, tabuleiro);
 		}
 	}
 
-	private char[][] montarTabuleiro(String[] linhas) {
+	private char[][] montarTabuleiro(String contents) {
+		
+		contents = contents.replace(" ", "");
+		
+		String[] linhas = contents.split("\n");
+		
 		// monta o tabuleiro de char[][]
 		char[][] tabuleiro = new char[linhas.length][];
 		for (int i = 0; i < linhas.length; i++) {
