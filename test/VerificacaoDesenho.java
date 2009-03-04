9e7f9b73b317cd5a452f7ad5d5b7ea12b5d7f14b
Bruno Pedroso <Bruno@macasa.local>
2009-03-04 17:45:04 -0300

    faltou adicionar esse teste no dojo de antes do carnaval

---
+++ test/VerificacaoDesenho.java
@@ -0,0 +1,34 @@
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+
+public class VerificacaoDesenho {
+	
+	@Test
+	public void desenho1(){
+		
+		String preProcessado = "____\n" +
+		  				  	   "B__B\n" +
+		  				  	   "B__B\n";
+		
+		String esperado = "#####################\n"+
+		  				  "#    #    #    #    #\n"+
+		  				  "#    #    #    #    #\n"+
+		  				  "#####################\n"+
+    		  			  "     #    #    #     \n"+
+		  				  "     #    #    #     \n"+
+		  				  "     ###########     \n"+
+		  				  "     #    #    #     \n"+
+		  				  "     #    #    #     \n"+
+		  				  "     ###########     \n";
+		
+		Puzzle puzzle = new Puzzle();
+		puzzle.loadGrid(preProcessado);
+		
+		Assert.assertEquals(esperado, puzzle.getCrosswords());
+						  
+		
+	}
+	
+}
